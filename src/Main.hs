956596fa8f7ab0f31080886e62ab923feb8d860d
Zeke Foppa <benjamin.foppa@gmail.com>
2016-11-30 19:58:54 -0500

    better error handling

--- src/Main.hs
+++ src/Main.hs
@@ -10,10 +10,14 @@ import Data.Time.Clock
 import Network.Curl
 import System.Environment
 import Text.Feed.Constructor
-import Text.Feed.Import
 import Text.Feed.Export
+import Text.Feed.Import
 import Text.Feed.Query
 import Text.Feed.Types
+
+infixl 1 |>
+(|>) :: a -> (a -> b) -> b
+(|>) x f = f x
 
 feeds :: String -> IO [String]
 feeds file = read <$> readFile file
@@ -25,35 +29,43 @@ fetch_url url = do
 
 get_feed :: String -> IO [(Feed, Item)]
 get_feed url = do
-  print ("Getting " ++ url)
+  putStrLn ("Getting " ++ url)
   feed <- fetch_url url
   case parseFeedString feed of
     Nothing -> do
-      print ("Error parsing " ++ url)
+      putStrLn ("Error parsing " ++ url)
       return []
     Just feed -> do
       return ((feed,) <$> feedItems feed)
 
-merge :: [[(Feed, Item)]] -> [(Feed, Item)]
-merge feeds = runIdentity $ do
+sortByCompareM :: (Ord b, Monad m) => (a -> m b) -> [a] -> m [a]
+sortByCompareM comparable l = do
+  comparable <- mapM comparable l
+  return $ map snd $ sortBy (compare `on` fst) (zip comparable l)
+
+merge :: [[(Feed, Item)]] -> IO [(Feed, Item)]
+merge feeds = do
     feeds <- return $ mapMaybe uncons feeds
-    feeds <- return $ sortBy (\x y -> negate_ord $ (compare `on` last_update) x y) feeds
+    feeds <- reverse <$> sortByCompareM last_update feeds
     case feeds of
       [] -> return []
       ((x, feed):feeds) -> do
         feeds <- return $ map (\(x, feed) -> x:feed) feeds
-        return $ x : merge (feed : feeds)
+        merged <- merge (feed : feeds)
+        return (x : merged)
   where
-    negate_ord EQ = EQ
-    negate_ord GT = LT
-    negate_ord LT = GT
-
-    last_update :: ((Feed, Item), [(Feed, Item)]) -> UTCTime
-    last_update ((_feed, item), _rest) = runIdentity $ do
-      let as_str = fromJust $ getItemPublishDateString item
-      case parse_published_time as_str of
-        Nothing -> error ("Could not parse publish time " ++ as_str)
-        Just x -> return x
+    last_update :: ((Feed, Item), [(Feed, Item)]) -> IO (Maybe UTCTime)
+    last_update ((_feed, item), _rest) = do
+      case getItemPublishDateString item of
+        Nothing -> do
+          putStrLn ("Could not find time for " ++ show item)
+          return Nothing
+        Just as_str -> do
+          case parse_published_time as_str of
+            Just x -> return (Just x)
+            Nothing -> do
+              putStrLn ("Could not parse publish time " ++ as_str)
+              return Nothing
 
     parse_published_time :: String -> Maybe UTCTime
     parse_published_time s = runIdentity $ do
@@ -65,12 +77,51 @@ merge feeds = runIdentity $ do
             ]
       return $ foldl mplus Nothing (map (\fmt -> parseTimeM True defaultTimeLocale fmt s) formats)
 
-merged_feed :: String -> IO [(Feed, Item)]
-merged_feed feeds_file = do
+merged_feed_items :: String -> IO [(Feed, Item)]
+merged_feed_items feeds_file = do
   feeds <- feeds feeds_file
   feeds <- mapM get_feed feeds
-  print "Finished fetching"
-  return $ merge feeds
+  putStrLn "Finished fetching"
+  merged <- merge feeds
+  return merged
+
+-- Add feed info to the author of the item
+amend_item_author :: Feed -> Item -> Item
+amend_item_author feed item = runIdentity $ do
+  feed_title <- return $ getFeedTitle feed
+  author <-
+    case getItemAuthor item of
+      Nothing -> return feed_title
+      Just author -> return (feed_title ++ ": " ++ author)
+  return $ withItemAuthor author item
+
+infixl 3 <?>
+(<?>) :: Maybe a -> a -> a
+Nothing <?> x = x
+Just x <?> _ = x
+
+-- Convert an item to a common type
+normalize_item :: Item -> Item
+normalize_item item =
+  newItem (RSSKind Nothing)
+  |> (withItemTitle <$> getItemTitle item <?> id)
+  |> (withItemLink <$> getItemLink item <?> id)
+  |> (withItemPubDate <$> getItemPublishDateString item <?> id)
+  |> (withItemAuthor <$> getItemAuthor item <?> id)
+  |> (withItemDescription <$> getItemDescription item <?> id)
+
+merged_feed :: String -> IO Feed
+merged_feed feeds_file = do
+  items <- merged_feed_items feeds_file
+  items <- return $
+    items
+    |> map (\(feed, item) -> amend_item_author feed item)
+    |> map normalize_item
+  feed <- return $
+    newFeed (RSSKind Nothing)
+    |> withFeedTitle "Merged feed"
+    |> withFeedItems items
+  return feed
 
 main :: IO ()
 main = do
@@ -78,13 +129,8 @@ main = do
   case args of
     [feeds_file] -> do
       feed <- merged_feed feeds_file
-      feed <- return $ runIdentity $ do
-        feed <- return $ snd <$> feed
-        feed <- return $ withFeedItems feed (newFeed (RSSKind Nothing))
-        feed <- return $ xmlFeed feed
-        return feed
-      print feed
+      putStrLn (show (xmlFeed feed))
       return ()
     _ -> do
-      print "Unexpected arguments"
+      putStrLn "Unexpected arguments"
       return ()
